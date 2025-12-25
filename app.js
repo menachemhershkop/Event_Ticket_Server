452899cbad4a31e8129fd5c7ad5514736cf501e0
mendy hershkop <hers.mm@gmail.com>
2025-12-25 11:22:37 +0200

    cerate a sale progrem, and reciipt system

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {promises as fs} from 'fs'
 import { userRout } from './rout/userRout.js'
 import { eventRout } from './rout/eventRout.js'
+import { ticketsRout } from './rout/ticketsRout.js'
 const app = express()
 const port = 3000
 app.use(express.json())
@@ -12,3 +13,4 @@ console.log(`server listening on port ${port}`)
 
 app.use('/user', userRout);
 app.use('/creator/event', eventRout)
+app.use('/users', ticketsRout)
--- rout/ticketsRout.js
+++ rout/ticketsRout.js
@@ -1,4 +1,46 @@
+import { log } from 'console'
 import express from 'express'
 import {promises as fs} from 'fs'
-export const userRout= express()
+export const ticketsRout= express()
 ticketsRout.use(express.json())
+
+const eventFile='data/events.json'
+const receiptFile = 'data/receipts.json'
+
+ticketsRout.post('/tickets/buy', async (req, res)=>{
+    const body = req.body
+    const event = JSON.parse(await fs.readFile(eventFile, "utf8"))
+    const receipts = JSON.parse(await fs.readFile(receiptFile, "utf8"))
+    let flag = false
+    try{
+        if ('eventName' in body && 'quantity' in body){
+            event.forEach((eve)=> {if (eve.eventName== body.eventName){
+                if (eve.ticketsForSale> body.quantity){
+                    eve.ticketsForSale -= body.quantity
+                    receipts.push({username:body.username, eventName:body.eventName, ticketsBought:body.quantity})
+                    fs.writeFile(eventFile, JSON.stringify(event));
+                    fs.writeFile(receiptFile, JSON.stringify(receipts));
+                    res.send({"message": "Tickets purchased successfully"})
+                }
+                else{
+                    res.status(400).send("Not enough tickets")
+                }
+
+                flag =true    
+            }
+        })
+    }
+    // console.log(event);
+    else{
+        res.status(400).send("Bad Request")
+    }
+    if (!flag){
+        res.status(400).send("Invalid event name. Try again.")
+    }
+}
+    catch(error){
+        console.log(error);
+        
+    }
+
+})
